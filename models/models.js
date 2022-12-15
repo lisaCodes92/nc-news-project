106c91669f3707f9f85cf15f369bf271291ec7d0
lisaCodes92 <lisamslloyd@gmail.com>
2022-12-15 13:27:15 +0000

    completes 6 GET /api/articles/article_id/comments

--- models/models.js
+++ models/models.js
@@ -40,7 +40,6 @@ exports.selectArticleComments = (articleId) => {
   WHERE article_id = $1
   ORDER BY created_at DESC `, [articleId])
     .then(({ rows: comments }) => {
-      console.log(comments)
       return comments;
     })
 };
